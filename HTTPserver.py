8ad58327e9a8f74abb803d9520a405b3d635bbfb
Matteo Simone Gagliano <msg177@clL28-13>
2020-03-02 20:33:28 +0100

    starting managing post request

--- HTTPserver.py
+++ HTTPserver.py
@@ -11,29 +11,36 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 	pass
 
 class MyHandler(BaseHTTPRequestHandler):
-    '''
-    def do_HEAD(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-    '''
     def do_GET(self):
-        if self.path == '':
-        	self.respond({'status': 200})
+        print('Managing GET request')
+        if self.path == '/' or self.path == '/index.html':
+        	print('Enter if')
+        	self.handle_http_GET(200, self.path)
         else:
-	        self.respond({'status': 500})
+	        pass
 
-    def handle_http(self, status_code, path):
-        f = open(curdir + '/index.html')
-        self.send_response(status_code)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        page = f.read()
-        f.close()
-        return bytes(page, 'UTF-8')
+    def do_POST(self):
+        print('Managing POST request')
+        if self.path == '/pay':
+	        pass
+        else:
+	        pass
 
-    def respond(self, opts):
-        response = self.handle_http(opts['status'], self.path)
+    def handle_http_GET(self, status_code, path):
+        if self.path == '/' or self.path == '/index.html':
+	        f = open(curdir + '/index.html')
+	        self.send_response(status_code)
+	        self.send_header('Content-type', 'text/html')
+	        self.end_headers()
+	        page = f.read()
+	        f.close()
+	        self.respond(bytes(page,'UTF-8'))
+
+    def handle_http_POST(self, status_code, path):
+	        pass
+
+
+    def respond(self, response):
         self.wfile.write(response)
 try:
 	httpd = ThreadedHTTPServer(('localhost', PORT), MyHandler)
